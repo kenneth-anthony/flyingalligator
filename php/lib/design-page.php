c41df3a0078a7b61346935025855fb1c255b0407
Kenneth-Anthony <kchavez.bills@gmail.com>
2015-09-08 22:44:48 -0600

    started an image gallery

--- php/lib/design-page.php
+++ php/lib/design-page.php
@@ -5,7 +5,7 @@ require_once"footer.php";
 
 <div class="container">
 <ul class="row">
-	<li class="col-lg-2 col-md-2 col-sm-xs-4"><img src=""> </li>
+	<li class="col-lg-2 col-md-2 col-sm-xs-4"><img src="">http://michaelsoriano.com/create-a-responsive-photo-gallery-with-bootstrap-framework/ </li>
 	<li> </li>
 	<li> </li>
 	<li> </li>
