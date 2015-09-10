3743872741792d8c276a2f837efe3222064debec
Kenneth Chavez <kchavez.bills@gmail.com>
2015-09-10 02:43:28 -0600

    Deleted old files again

--- photography/street-photography.php
+++ photography/street-photography.php
@@ -7,6 +7,7 @@ require_once(dirname(__DIR__) . "/php/lib/head-utils.php");
 <body class="sfooter">
 	<div class="sfooter-content">
 		<div class="design-background">
+
 			<header>
 				<?php require_once(dirname(__DIR__) . "/php/lib/header.php"); ?>
 			</header>
