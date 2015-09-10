5351a8f905f20229570a7047ac4c5ea3c5a69388
Kenneth-Anthony <kchavez.bills@gmail.com>
2015-09-10 00:45:40 -0600

    Cleaned up css and updated small images

--- design/index.php
+++ design/index.php
@@ -12,7 +12,57 @@ require_once(dirname(__DIR__) . "/php/lib/head-utils.php");
 			</header>
 			<main>
 				<div class="container">
-					<a href="big"
+					<div class="row-one">
+						<div class="col-md-4">
+							<a href="/media/haaku-me/haaku-1-lg.jpg" class="swipebox" title="Haakume Exhibit">
+								<img src="/media/haaku-me/haaku-1-sm.jpg" alt="image">
+							</a>
+						</div>
+						<div class="col-md-4">
+							<a href="/media/haaku-me/haaku-2-lg.jpg" class="swipebox" title="Haakume Exhibit">
+								<img src="/media/haaku-me/haaku-2-sm.jpg" alt="image">
+							</a>
+						</div>
+						<div class="col-md-4">
+							<a href="/media/haaku-me/haaku-3-lg.jpg" class="swipebox" title="Haakume Exhibit">
+								<img src="/media/haaku-me/haaku-3-sm.jpg" alt="image">
+							</a>
+						</div>
+					</div>
+					<div class="row-two">
+						<div class="col-md-4">
+							<a href="/media/haaku-me/haaku-4-lg.jpg" class="swipebox" title="Haakume Exhibit">
+								<img src="/media/haaku-me/haaku-4-sm.jpg" alt="image">
+							</a>
+						</div>
+						<div class="col-md-4">
+							<a href="/media/haaku-me/haaku-6-lg.jpg" class="swipebox" title="Haakume Exhibit">
+								<img src="/media/haaku-me/haaku-6-sm.jpg" alt="image">
+							</a>
+						</div>
+						<div class="col-md-4">
+							<a href="/media/haaku-me/haaku-7-lg.jpg" class="swipebox" title="Haakume Exhibit">
+								<img src="/media/haaku-me/haaku-7-sm.jpg" alt="image">
+							</a>
+						</div>
+					</div>
+					<div class="row-three">
+						<div class="col-md-4">
+							<a href="/media/haaku-me/haaku-8-lg.jpg" class="swipebox" title="Haakume Exhibit">
+								<img src="/media/haaku-me/haaku-8-sm.jpg" alt="image">
+							</a>
+						</div>
+						<div class="col-md-4">
+							<a href="/media/haaku-me/haaku-9-lg.jpg" class="swipebox" title="Haakume Exhibit">
+								<img src="/media/haaku-me/haaku-9-sm.jpg" alt="image">
+							</a>
+						</div>
+						<div class="col-md-4">
+							<a href="/media/haaku-me/haaku-10-lg.jpg" class="swipebox" title="Haakume Exhibit">
+								<img src="/media/haaku-me/haaku-10-sm.jpg" alt="image">
+							</a>
+						</div>
+					</div>
 				</div>
 			</main>
 		</div>
